caa80c9320080fede083f4c50d6857ceee793dbe
nbtuyen <tuyennbph13946@fpt.edu.vn>
2022-07-30 21:00:09 +0700

    aa

---
+++ blocks/product_new/view/product_new.php
@@ -0,0 +1,160 @@
+<link rel="stylesheet" href="blocks/product_new/asset/css/product_new.css">
+<section class="awe-section-4">
+    <section class="section_product_news">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="title_module_bbf">
+                        <h2 class="bf_flower">
+                            <a href="onsale" title="Sản phẩm mới">Sản phẩm mới</a>
+                        </h2>
+                        <span class="destitle">Bộ sưu tập sản phẩm mới nhất từ Delta Cosmetic</span>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="section">
+                        <div class="owl-carousel">
+                            <div class="item_product_main">
+                                <div class="product-thumbnail">
+                                    <span class="sale_label" style="background-image: url('	https://theme.hstatic.net/200000230435/1000666031/14/label_sale.svg?v=28');">-
+                                        22%
+                                    </span>
+                                    <a class="image_thumb thumb_product_index" href="/products/kem-chong-nang-mua-he-gga" title="Đông trùng hạ thảo Aloha">
+                                        <img src="//product.hstatic.net/200000230435/product/upload_8c3652719bbc40a189fc83de573709ec_large.jpg" data-lazyload="//product.hstatic.net/200000230435/product/upload_8c3652719bbc40a189fc83de573709ec_large.jpg" alt="Đông trùng hạ thảo Aloha">
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <h3 class="product-name"><a href="/products/kem-chong-nang-mua-he-gga" title="Đông trùng hạ thảo Aloha">Đông trùng hạ thảo Aloha</a></h3>
+                                    <div class="price-box">
+                                        <span class="price product-price">380,000₫</span>
+                                        <span class="compare-price">490,000₫</span>
+                                    </div>
+                                    <form action="/cart/add" method="post" class="variants form-nut-grid hidden-xs" data-id="product-actions-1028406521" enctype="multipart/form-data">
+                                        <div class="group_action">
+                                            <input type="hidden" name="Id" value="1062098290">
+                                            <a class="button_add" title="Thêm vào giỏ">
+                                                Thêm vào giỏ
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="item_product_main">
+                                <div class="product-thumbnail">
+                                    <a class="image_thumb thumb_product_index" href="/products/moonshadow-baked-palette" title="Phấn mắt Moonshadow Baked">
+                                        <img src="//product.hstatic.net/200000230435/product/upload_d889c47cff864b70ae31467ef119da52_large.jpg" data-lazyload="//product.hstatic.net/200000230435/product/upload_d889c47cff864b70ae31467ef119da52_large.jpg" alt="Phấn mắt Moonshadow Baked">
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <h3 class="product-name"><a href="/products/moonshadow-baked-palette" title="Phấn mắt Moonshadow Baked">Phấn mắt Moonshadow Baked</a></h3>
+                                    <div class="price-box">
+                                        <span class="price product-price">679,000₫</span>
+                                    </div>
+                                    <form action="/cart/add" method="post" class="variants form-nut-grid hidden-xs" data-id="product-actions-1028406463" enctype="multipart/form-data">
+                                        <div class="group_action">
+                                            <input type="hidden" name="Id" value="1062098207">
+                                            <a class="button_add add_to_cart" title="Thêm vào giỏ">
+                                                Thêm vào giỏ
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="item_product_main">
+                                <div class="product-thumbnail">
+                                    <a class="image_thumb thumb_product_index" href="/products/phan-nen-bareminerals-original-foundation-broad-spectrum" title="Phấn nền bareMinerals Original">
+                                        <img src="//product.hstatic.net/200000230435/product/upload_52d095c9f6d94d5abc1fbd3a963b2ac9_large.jpg" data-lazyload="//product.hstatic.net/200000230435/product/upload_52d095c9f6d94d5abc1fbd3a963b2ac9_large.jpg" alt="Phấn nền bareMinerals Original">
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <h3 class="product-name"><a href="/products/phan-nen-bareminerals-original-foundation-broad-spectrum" title="Phấn nền bareMinerals Original">Phấn nền bareMinerals Original</a></h3>
+                                    <div class="price-box">
+                                        <span class="price product-price">560,000₫</span>
+                                    </div>
+                                    <form action="/cart/add" method="post" class="variants form-nut-grid hidden-xs" data-id="product-actions-1028406468" enctype="multipart/form-data">
+                                        <div class="group_action">
+                                            <input class="hidden" type="hidden" name="Id" value="1062098215">
+                                            <a class="button_add" title="Nhiều lựa chọn" href="/products/phan-nen-bareminerals-original-foundation-broad-spectrum">
+                                                Nhiều lựa chọn
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="item_product_main">
+                                <div class="product-thumbnail">
+                                    <span class="sale_label">-
+                                        17%
+                                    </span>
+                                    <a class="image_thumb thumb_product_index" href="/products/kem-duong-da-ban-ngay-a5" title="Se khít lỗ chân lông Murad Blackhead">
+                                        <img src="//product.hstatic.net/200000230435/product/upload_613818d84339438eb418ec59d25d9e87_large.jpg" data-lazyload="//product.hstatic.net/200000230435/product/upload_613818d84339438eb418ec59d25d9e87_large.jpg" alt="Se khít lỗ chân lông Murad Blackhead">
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <h3 class="product-name"><a href="/products/kem-duong-da-ban-ngay-a5" title="Se khít lỗ chân lông Murad Blackhead">Se khít lỗ chân lông Murad Blackhead</a></h3>
+                                    <div class="price-box">
+                                        <span class="price product-price">120,000₫</span>
+                                        <span class="compare-price">145,000₫</span>
+                                    </div>
+                                    <form action="/cart/add" method="post" class="variants form-nut-grid hidden-xs" data-id="product-actions-1028406523" enctype="multipart/form-data">
+                                        <div class="group_action">
+                                            <input type="hidden" name="Id" value="1062098292">
+                                            <a class="button_add add_to_cart" title="Thêm vào giỏ">
+                                                Thêm vào giỏ
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="item_product_main">
+                                <div class="product-thumbnail">
+                                    <a class="image_thumb thumb_product_index" href="/products/son-thoi-li-3ce-lip-color-matte-907" title="Son thỏi lì 3ce Lip color Matte - #907">
+                                        <img src="//product.hstatic.net/200000230435/product/upload_530dc00947bd4535a837d59dc9b88898_large.jpg" data-lazyload="//product.hstatic.net/200000230435/product/upload_530dc00947bd4535a837d59dc9b88898_large.jpg" alt="Son thỏi lì 3ce Lip color Matte - #907">
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <h3 class="product-name"><a href="/products/son-thoi-li-3ce-lip-color-matte-907" title="Son thỏi lì 3ce Lip color Matte - #907">Son thỏi lì 3ce Lip color Matte - #907</a></h3>
+                                    <div class="price-box">
+                                        <span class="price product-price">270,000₫</span>
+                                    </div>
+                                    <form action="/cart/add" method="post" class="variants form-nut-grid hidden-xs" data-id="product-actions-1028406460" enctype="multipart/form-data">
+                                        <div class="group_action">
+                                            <input type="hidden" name="Id" value="1062098202">
+                                            <a class="button_add add_to_cart" title="Thêm vào giỏ">
+                                                Thêm vào giỏ
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="item_product_main">
+                                <div class="product-thumbnail">
+                                    <a class="image_thumb thumb_product_index" href="/products/sua-tam-duong-am-cho-da" title="Sữa tắm dưỡng ẩm cho da">
+                                        <img src="//product.hstatic.net/200000230435/product/upload_ea4508e8491345f9ba8f2e30cb438e66_large.jpg" data-lazyload="//product.hstatic.net/200000230435/product/upload_ea4508e8491345f9ba8f2e30cb438e66_large.jpg" alt="Sữa tắm dưỡng ẩm cho da">
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <h3 class="product-name"><a href="/products/sua-tam-duong-am-cho-da" title="Sữa tắm dưỡng ẩm cho da">Sữa tắm dưỡng ẩm cho da</a></h3>
+                                    <div class="price-box">
+                                        <span class="price product-price">600,000₫</span>
+                                    </div>
+                                    <form action="/cart/add" method="post" class="variants form-nut-grid hidden-xs" data-id="product-actions-1028406476" enctype="multipart/form-data">
+                                        <div class="group_action">
+                                            <input class="hidden" type="hidden" name="Id" value="1062098235">
+                                            <a class="button_add" title="Nhiều lựa chọn" href="/products/sua-tam-duong-am-cho-da">
+                                                Nhiều lựa chọn
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
+
+<script src="blocks/product_new/asset/js/product_new.js"></script>
---
+++ blocks/review/view/review.php
@@ -0,0 +1,106 @@
+<link rel="stylesheet" href="blocks/review/asset/css/review.css">
+
+<section class="awe-section-5" style="background: url('https://theme.hstatic.net/200000230435/1000666031/14/bg-rate.jpg?v=28') center no-repeat;">
+    <section class="section_npp_reviews">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="title_module_bbf">
+                        <h2 class="bf_flower">
+                            <span>Nhà phân phối tiêu biểu và đánh giá khách hàng</span>
+                        </h2>
+                        <span class="destitle">Danh sách các nhà phân phối, các đại lý tiêu biểu và đánh giá của khách hàng về sản phẩm của Delta Cosmetic Việt Nam</span>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <h2 class="title_normal">
+                                <span>Danh sách nhà phân phối &amp; đại lý tiêu biểu</span>
+                            </h2>
+                            <div class="box">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Tên</th>
+                                            <th>Cấp bậc</th>
+                                            <th>Khu vực</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>Lê Việt Nga</th>
+                                            <th>NPP Vàng</th>
+                                            <th>Đà Nẵng</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Nguyễn Thị Hằng</th>
+                                            <th>NPP</th>
+                                            <th>Q. Bình Tân</th>
+                                        </tr>
+
+                                        <tr>
+                                            <th>Nguyễn Thanh Thảo</th>
+                                            <th>NPP</th>
+                                            <th>Đà Nẵng</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Thùy Linh</th>
+                                            <th>Đại Lý</th>
+                                            <th>Hà Nội</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Hoa Lan</th>
+                                            <th>Đại lý</th>
+                                            <th>Q. Gò Vấp</th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-lg-5">
+                            <h2 class="title_normal">
+                                <span>Nhận xét của khách hàng</span>
+                            </h2>
+                            <div class="box">
+                                <ul class="owl-carousel">
+                                    <li>
+                                        <img src="//theme.hstatic.net/200000230435/1000666031/14/image_dg_1.jpg?v=31" data-lazyload="//theme.hstatic.net/200000230435/1000666031/14/image_dg_1.jpg?v=31" alt="Nguyễn Thảo Mai">
+                                        <span class="sub-header">Khách hàng</span>
+                                        <h5>
+                                            Nguyễn Thảo Mai
+                                        </h5>
+                                        <p class="paragraph-with-quote">
+                                            Mỹ phẩm thiên nhiên Delta Cosmetic nhà mình ai cũng dùng. Trải nghiệm quá sức ngoài mong đợi.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <img src="//theme.hstatic.net/200000230435/1000666031/14/image_dg_2.jpg?v=31" data-lazyload="//theme.hstatic.net/200000230435/1000666031/14/image_dg_2.jpg?v=31" alt="Trà Nguyễn">
+                                        <span class="sub-header">Khách hàng</span>
+                                        <h5>
+                                            Trà Nguyễn
+                                        </h5>
+                                        <p class="paragraph-with-quote">
+                                            Sản phẩm rất tốt từ serum, kem phấn tới son em đều thích.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <img src="//theme.hstatic.net/200000230435/1000666031/14/rolling.svg?v=31" data-lazyload="//theme.hstatic.net/200000230435/1000666031/14/image_dg_3.jpg?v=31" alt="Thu Trang">
+                                        <span class="sub-header">Khách hàng</span>
+                                        <h5>
+                                            Thu Trang
+                                        </h5>
+                                        <p class="paragraph-with-quote">
+                                            Sản phẩm dùng rất tốt, hợp cho mọi lứa tuổi, 100% thiên nhiên, rất an toàn”.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
+<script src="blocks/review/asset/js/review.js"></script>
